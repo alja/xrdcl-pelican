861520ce20da5ceaeb695388e6cd84cec073e5a3
alja <amraktadel@ucsd.edu>
2025-05-29 21:41:08 -0700

    Add error messages in Fcntl for unhandled query codes

--- src/XrdClCurl/CurlFile.cc
+++ src/XrdClCurl/CurlFile.cc
@@ -304,6 +304,45 @@ File::Fcntl(const XrdCl::Buffer &arg, XrdCl::ResponseHandler *handler,
             respBuff->FromString(xatt.dump());
             obj->Set(respBuff);
         }
+        //
+        //   Query codes supported by  XrdCl::File::Fctnl
+        //
+        else if (code == XrdCl::QueryCode::Stats)
+        {
+             m_logger->Error(kLogXrdClCurl, "Server status query not supported.");
+             return XrdCl::XRootDStatus(XrdCl::stError, XrdCl::errInvalidOp);
+        }
+        else if (code == XrdCl::QueryCode::Checksum || code == XrdCl::QueryCode::ChecksumCancel)
+        {
+             m_logger->Error(kLogXrdClCurl, "Checksum query not supported.");
+             return XrdCl::XRootDStatus(XrdCl::stError, XrdCl::errInvalidOp);
+        }
+        else if (code == XrdCl::QueryCode::Config)
+        {
+            m_logger->Error(kLogXrdClCurl, "Server configuration query not supported.");
+             return XrdCl::XRootDStatus(XrdCl::stError, XrdCl::errInvalidOp);
+        }
+        else if (code == XrdCl::QueryCode::Space)
+        {
+            m_logger->Error(kLogXrdClCurl, "Local space stats query not supported.");
+             return XrdCl::XRootDStatus(XrdCl::stError, XrdCl::errInvalidOp);
+        }
+        else if (code == XrdCl::QueryCode::Opaque || code == XrdCl::QueryCode::OpaqueFile)
+        {
+            // XrdCl implementation dependent
+            m_logger->Error(kLogXrdClCurl, "Opaque query not supported.");
+             return XrdCl::XRootDStatus(XrdCl::stError, XrdCl::errInvalidOp);
+        }
+        else if (code == XrdCl::QueryCode::Prepare)
+        {
+            m_logger->Error(kLogXrdClCurl, "Prepare status query not supported.");
+             return XrdCl::XRootDStatus(XrdCl::stError, XrdCl::errInvalidOp);
+        }
+        else
+        {
+            m_logger->Error(kLogXrdClCurl, "Invalid information query type code");
+            return XrdCl::XRootDStatus(XrdCl::stError, XrdCl::errInvalidArgs);
+        }
     }
     catch (const std::exception& e)
     {
